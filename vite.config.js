091f24f5fe6cf7b60c2f34a6ea3959df93757d6f
JMORAF87 <j.makatheprofessor@gmail.com>
2026-01-21 21:20:04 -0800

    Configure rewrites in vite.config.js
    Added rewrites for API and root path handling.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+{
+  "rewrites": [
+    {
+      "source": "/api/:path*",
+      "destination": "https://elite24-api.onrender.com/api/:path*"
+    },
+    {
+      "source": "/(.*)",
+      "destination": "/"
+    }
+  ]
+}
